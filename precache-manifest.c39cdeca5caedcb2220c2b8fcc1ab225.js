42cf68e042ad96aa79a49b277e6a9f54c4f001bb
sanyavanya <ivanov.unn@gmail.com>
2020-06-09 17:56:32 +0300

    Updates

---
+++ precache-manifest.c39cdeca5caedcb2220c2b8fcc1ab225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e829e332a60c027f1d6cb41ce2973a",
+    "url": "/github-dashboard/index.html"
+  },
+  {
+    "revision": "e1f2216e2a03abf440e0",
+    "url": "/github-dashboard/static/css/main.3c565159.chunk.css"
+  },
+  {
+    "revision": "c093ffb21aa07c544a06",
+    "url": "/github-dashboard/static/js/2.589bf15b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-dashboard/static/js/2.589bf15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f2216e2a03abf440e0",
+    "url": "/github-dashboard/static/js/main.ffadf76f.chunk.js"
+  },
+  {
+    "revision": "46287b397d1ff160f3a4",
+    "url": "/github-dashboard/static/js/runtime-main.4926da2c.js"
+  },
+  {
+    "revision": "23616449155fd33561d24f262216abb0",
+    "url": "/github-dashboard/static/media/spinner.23616449.png"
+  }
+]);
