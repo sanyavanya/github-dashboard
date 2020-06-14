0456768958948c3b17e27e7881acc82d91a16394
sanyavanya <ivanov.unn@gmail.com>
2020-06-14 12:10:10 +0300

    Improved readability for Paginator.js

--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import "./Paginator.css"
+import "./Paginator.css";
 
-const Paginator = ({pagesAmount, page, onPageChange}) => {
+const Paginator = ({ pagesAmount, page, onPageChange }) => {
 	let paginator = [];
 	if (pagesAmount > 1) {
-		paginator = [<span key="leftbracket">[ </span>];
-		
+		paginator = [<span key="leftbracket">[ </span>];		
 		for (let i = 1; i <= pagesAmount; i++) {
 			let linkStyle = 'pageLink';
 			if (i === page) linkStyle += ' current';
 			let newPageLink = [
-	            <li key ={i} className={linkStyle} onClick={onPageChange} style={{display: "inline"}}>
-	              {i}
-	            </li>
+	            <li key ={i} className={linkStyle} onClick={onPageChange} style={{display: "inline"}}>{i}</li>
 	          ];
 	        let separator = [<span key={`separator ${i}`}>, </span>];
 	        if (i < pagesAmount) newPageLink = newPageLink.concat(separator);
 			paginator = paginator.concat(newPageLink);
 	    }
-
-	        paginator = paginator.concat(<span key="rightbracket"> ]</span>);
+	    paginator = paginator.concat(<span key="rightbracket"> ]</span>);
     }
-
-
-
-
 
 	return (
 		<div id='paginator'>{paginator}</div>
